fbdc242849340fba278a9c241180115bb38436e9
alperengokbak <swalperen3008@gmail.com>
2024-04-30 00:37:42 +0300

    Feature: Created config file.

---
+++ config/corsOptions.js
@@ -0,0 +1,13 @@
+const whitelist = ["https://www.google.com", "http://localhost:8080"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+export default corsOptions;
