dc2cb9d54c663d5ef9bbd15aae4087a913da6aec
alperengokbak <swalperen3008@gmail.com>
2024-05-01 12:42:08 +0300

    Refactoring: Updated authentication function.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,12 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.header["authorization"];
-  if (!token) return res.status(401).json({ status: "Unauthorized" });
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.status(401).json({ status: "Unauthorized" });
+
   try {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) return res.status(401).json({ status: "Unauthorized" });
-      req.user = decoded;
+      if (err) return res.status(403).json({ status: "Invalid Token" });
+      req.user = decoded.email;
       next();
     });
   } catch (error) {
